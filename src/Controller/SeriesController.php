3f75637cd6a3e68db23fc5f2fea21f06177c5784
Fabio <fabinhoizzy@iMac-Pro-de-Fabio.local>
2022-12-12 21:02:17 -0300

    Criando um formulario com symfony Form

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Series;
 use App\Repository\SeriesRepository;
-use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,19 +23,23 @@ class SeriesController extends AbstractController
     }
 
     #[Route('/series', name: 'app_series', methods: 'GET')]
-    public function seriesList(): Response
+    public function seriesList(Request $request): Response
     {
         $seriesList = $this->seriesRepository->findAll();
 
         return $this->render('series/index.html.twig', [
-            'seriesList' => $seriesList
+            'seriesList' => $seriesList,
         ]);
 
     }
     #[Route('/series/create', name: 'app_series_form', methods: ['GET'])]
     public function addSeriesForm(): Response
     {
-        return $this->render('series/form.html.twig');
+        $seriesForm = $this->createFormBuilder(new Series(''))
+            ->add('name', TextType::class, ['label' => 'Nome: '])
+            ->add('Adicionar', SubmitType::class,['label' => 'Adicionar'])
+            ->getForm();
+        return $this->renderForm('series/form.html.twig', compact('seriesForm'));
     }
 
     #[Route('/series/create',name: 'app_add_series', methods: ['POST'])]
@@ -41,16 +47,33 @@ class SeriesController extends AbstractController
     {
        $seriesName = $request->request->get('name');
        $series = new Series($seriesName);
+       $this->addFlash('success', "Série \"{$seriesName}\" adicionada com sucesso");
 
        $this->seriesRepository->save($series, true);
        return new RedirectResponse('/series');
     }
 
-    #[Route('/series/delete/{id}', methods: ['POST'])]
-    public function deleteSeries(int $id): Response
+    #[Route('/series/delete/{id}', name: 'app_delete_series', requirements: ['id' => '[0-9]+'], methods: ['DELETE'])]
+    public function deleteSeries(int $id, Request $request): Response
     {
-        $series = $this->entityManager->getPartialReference(Series::class,  $id);
-        $this->seriesRepository->remove($series, true);
+        $this->seriesRepository->removeById($id);
+        $this->addFlash('success', "Série removida com sucesso");
+
+        return new RedirectResponse('/series');
+    }
+
+    #[Route('/series/edit/{series}', name: 'app_edit_series_form', methods: ['GET'])]
+    public function editSeriesForm(Series $series): Response
+    {
+        return $this->render('series/form.html.twig', compact('series'));
+    }
+
+    #[Route('/series/edit/{series}', name: 'app_store_series_changes' ,methods: ['PATCH'])]
+    public function storeSeriesChanges(Series $series, Request $request): Response
+    {
+        $series->setName($request->request->get('name'));
+        $this->addFlash('success', "Série \"{$series->getName()}\" editada com sucesso");
+        $this->entityManager->flush();
 
         return new RedirectResponse('/series');
     }
